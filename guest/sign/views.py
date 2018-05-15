16eb1df9376361a577425dabe19eb2a1bc80cbb8
Yvonne160 <farmerhuo@163.com>
2018-05-15 12:52:47 +0800

    处理post方式的登录请求

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -21,3 +21,15 @@ from django.http import HttpResponse
 #post 请求
 def index(request):
     return render(request,'index.html')
+
+
+#处理登录请求
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('user','')
+        password = request.POST.get('pw','')
+        if username=="admin" and password=="admin123":
+            return HttpResponse('login success!')
+        else:
+            return render(request,'index.html',{'error':'用户名或密码错误'})
+
